d04c60cc11e9a54cd7bb9000d62d6e3caa9a5a19
Fatmaerdogan <erdoganfatma266@gmail.com>
2019-03-06 12:15:08 +0300

    CatUpload

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+    public GameObject ıdle;
+    public GameObject walk;
+    public GameObject sound;
+    public GameObject eat;
+    public GameObject jump;
+    private void Start()
+    {
+        sound.SetActive(false);
+        walk.SetActive(false);
+        eat.SetActive(false);
+        jump.SetActive(false);
+    }
+    public void Jump()
+    {
+        ıdle.SetActive(false);
+        sound.SetActive(false);
+        walk.SetActive(false);
+        eat.SetActive(false);
+        jump.SetActive(true);
+    }
+    public void Sound()
+    {
+        ıdle.SetActive(false);
+        sound.SetActive(true);
+        walk.SetActive(false);
+        eat.SetActive(false);
+        jump.SetActive(false);
+    }
+    public void Walk()
+    {
+        ıdle.SetActive(false);
+        sound.SetActive(false);
+        walk.SetActive(true);
+        eat.SetActive(false);
+        jump.SetActive(false);
+    }
+    public void Idle()
+    {
+        ıdle.SetActive(true);
+        sound.SetActive(false);
+        walk.SetActive(false);
+        eat.SetActive(false);
+        jump.SetActive(false);
+    }
+    public void Eat()
+    {
+        ıdle.SetActive(false);
+        sound.SetActive(false);
+        walk.SetActive(false);
+        eat.SetActive(true);
+        jump.SetActive(false);
+    }
+}
